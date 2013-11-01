d9d36f9a129fece6030d4e369a4411c996b38630
gushuro <gushurovich@gmail.com>
2013-11-01 16:56:13 -0200

    Update pelicula.cpp

--- pelicula.cpp
+++ pelicula.cpp
@@ -1,6 +1,6 @@
 #include "pelicula.h"
  
-Pelicula::nuevaP (Nombre n, Lista<Genero> gs, Lista<Actor> as, bool b) {
+Pelicula::Pelicula (Nombre n, Lista<Genero> gs, Lista<Actor> as, bool b) {
   nombre_ = n;
   generos_ = gs;
   actores_ = as;
@@ -24,11 +24,6 @@ bool Pelicula::es3DP() const {
   return es3D_;
 }
 
-Lista<(Genero,Lista<Pelicula>)> Pelicula::agruparPelisPorGeneroP (Lista<Pelicula> ps) {
-  Lista<Genero> losGeneros = agregar(agregar(agregar(agregar(agregar(Terror,Lista<Generos>),Drama),Comedia),Aventura),Acción)
-  Lista<(Genero,Lista<Pelicula>)> res = Lista<>
-  
-  while(losGeneros.longitud > 0) {
-   
-  }
-}
+Lista<std::pair<Genero,Lista<Pelicula> > > Pelicula::agruparPelisPorGeneroP(Lista<Pelicula> ps) const{
+
+};

ffb9f7c568d31bc4853e033c0f47a77a87bebfab
gushuro <gushurovich@gmail.com>
2013-11-03 16:07:48 -0200

    Update cine.cpp

--- cine.cpp
+++ cine.cpp
@@ -4,5 +4,37 @@ using namespace std;
 
 Cine::Cine(Nombre n) {
   nombre_ = n;
+  peliculas_ = Lista();
+  ticketsVendidos_ = Lista();
+  salasSinUsar_ = Lista();
 }
 
+Nombre Cine::nombreC() {
+  return nombre_;
+}
+
+Lista<Pelicula> Cine::peliculasC() {
+  Lista<Pair<Pelicula, Sala> > res;
+  t = peliculas_.longitud;
+  i = 0;
+  while (i < t) {
+    res.agregarAtras(peliculas_.iesimo(i).first);
+    i++;
+  }
+  return res;
+}
+
+Lista<Sala> Cine::salasC() {
+  Lista<Pair<Pelicula, Sala> > res;
+  t = peliculas_.longitud;
+  i = 0;
+  while (i < t) {
+    res.agregarAtras(peliculas_.iesimo(i).second);
+    i++;
+  }
+  return res;
+}
+
+int Cine::espectadoresC(Sala s) {
+  
+}
